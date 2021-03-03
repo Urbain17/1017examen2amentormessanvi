ff9a9dc5101addac5b7d7831561e5ce47c263665
Urbain17 <amentor.messanvi.01@edu.cegeptr.qc.ca>
2021-03-02 21:53:06 -0500

    projet final

---
+++ src/specs/translationMixins.spec.js
@@ -0,0 +1,13 @@
+import { from } from 'core-js/fn/array'
+import Vue from 'vue'
+
+
+describe({'Profil.vue': () => {
+    it('should render correct contents'), () => {
+        const Contructeur = Vue.extend(age)
+        const vm = new Constructor().age()
+        expect(vm.$el.querySelector('.age').textContent).tobe(ager)
+    }
+},
+
+})
